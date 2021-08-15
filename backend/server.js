a27f86a411260fa76796c34a63d640d112d19883
OnikiriNoKami <butenko.ily@gmail.com>
2021-08-15 14:00:56 +0300

    json,fileupload,cors prep

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 require('dotenv').config()
 const express = require("express")
+const cors = require("cors")
+const fileUpload = require('express-fileupload')
+const path = require('path')
 const sequelize = require('./db')
 const PORT = process.env.PORT || 4221
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
+
 
 const start = async () => {
 
