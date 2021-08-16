d3cd616bc4ad9a1b3f2e50585c2df7da62ba5a4f
OnikiriNoKami <butenko.ily@gmail.com>
2021-08-16 16:42:10 +0300

    tank-type CRUD implementation

---
+++ backend/controllers/tankTypeController.js
@@ -0,0 +1,52 @@
+const { TankType } = require("../models/models");
+
+class TankTypeController {
+    async getAll(req, res) {
+        const tankTypes = await TankType.findAll()
+        return res.json(tankTypes)
+    }
+    
+    async create(req, res) {
+        const {title, title_short} = req.body
+        const tantType = await TankType.create({title, title_short})
+        return res.json(tankType)
+    }
+
+    async update(req, res){
+        const {id, title, title_short} = req.body
+        const tankType = await TankType.findOne({
+            where: {
+                id: id
+            }
+        })
+
+        if(tankType === null){
+            return res.json({message:"No such instance in database!"})
+        } else {
+            tankType.title = title
+            tankType.title_short = title_short
+            tankType.save()
+            return res.json(tankType)
+        }
+    }
+
+    async delete(req, res){
+        const {id} = req.body
+        const tankType = await TankType.findOne({
+            where: {
+                id: id
+            }
+        })
+
+        if(tankType === null){
+            return res.json({message:"No such instance in database!"})
+        } else {
+            tankType.destroy()
+            return res.json({message: "Success! Instance gone."})
+        }
+    }
+
+
+}
+
+module.exports = new TankTypeController()
--- backend/routes/tankTypeRoute.js
+++ backend/routes/tankTypeRoute.js
@@ -1,9 +1,10 @@
 const Router = require('express')
+const tankTypeController = require('../controllers/tankTypeController')
 const router = new Router()
 
-router.get('/',)
-router.post('/',)
-router.put('/',)
-router.delete('/',)
+router.get('/', tankTypeController.getAll)
+router.post('/', tankTypeController.create)
+router.put('/', tankTypeController.update)
+router.delete('/', tankTypeController.delete)
 
 module.exports = router
