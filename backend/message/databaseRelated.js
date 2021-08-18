6ee9e950e2ac8f91e72740a2b36ea26063b5b448
OnikiriNoKami <butenko.ily@gmail.com>
2021-08-18 13:50:17 +0300

    messages

---
+++ backend/message/databaseRelated.js
@@ -0,0 +1,5 @@
+module.exports = {
+    NOT_IN_DATABASE: "No such instance in database!",
+    DELETION_SUCCESS: "Instance was deleted from database!",
+    BASKET_NOT_FOUND: "No such basket in database!"
+}
