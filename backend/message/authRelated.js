8b7d57d05f87f138b1d8fc03bf4e8dfe7c57b440
OnikiriNoKami <butenko.ily@gmail.com>
2021-08-18 14:58:04 +0300

    auth middlware improvements

---
+++ backend/message/authRelated.js
@@ -0,0 +1,5 @@
+module.exports = {
+    NOT_AUTHORIZED: "Not authorized",
+    FAIL: "Authorization failed.",
+    NO_TOKEN: "No authorization token."
+}
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,19 +1,25 @@
 const jwt = require('jsonwebtoken')
+const messages = require('../message/authRelated')
 
 module.exports = function (req, res, next){
     if(req.method === "OPTIONS"){
         next()
     }
     try {
-        const parseToken = req.headers.authorization.split(' ')
-        const tokenType = parseToken[0]
-        const token = parseToken[1]
-        if(!token) {
-            return res.json({message: 'Woah not authorized!'})
+        if(req.headers.authorization)
+        {
+            const parseToken = req.headers.authorization.split(' ')
+            const tokenType = parseToken[0]
+            const token = parseToken[1]
+            if(!token) {
+                return res.status(401).json({message: messages.NOT_AUTHORIZED})
+            }
+            const decoded = jwt.verify(token, process.env.SECRET_JWT_KEY)
+            req.user = decoded
+            next()
+        } else {
+            return res.status(401).json({message: messages.NO_TOKEN})
         }
-        const decoded = jwt.verify(token, process.env.SECRET_JWT_KEY)
-        req.user = decoded
-        next()
     } catch (e) {
         return res.json({message: e.message})
     }
