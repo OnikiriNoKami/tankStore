b8dc61ca473b6e8d344af70c955f2ec4d984b83b
OnikiriNoKami <butenko.ily@gmail.com>
2021-08-18 15:22:14 +0300

    image routes setup with controller

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -50,3 +50,5 @@ class ImageController {
         }
     }
 }
+
+module.exports = new ImageController()
--- backend/routes/imageRoute.js
+++ backend/routes/imageRoute.js
@@ -1,10 +1,13 @@
 const Router = require('express')
+const imageController = require('../controllers/imageController')
+const authMiddleware = require('../middleware/authMiddleware')
 const router = new Router()
 
-router.get('/',)
-router.get('/:tankId',)
-router.post('/',)
-router.delete('/',)
+
+
+router.get('/:tankId', imageController.getAllById)
+router.post('/', authMiddleware, imageController.create)
+router.delete('/', authMiddleware, imageController.delete)
 
 
 module.exports = router
