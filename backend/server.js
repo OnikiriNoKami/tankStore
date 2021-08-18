2521ce0cff24f12ac55e03e9b8a200853b42b903
OnikiriNoKami <butenko.ily@gmail.com>
2021-08-18 13:42:54 +0300

    errorHandler mid in server

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,18 @@ const sequelize = require('./db')
 const models = require('./models/models.js')
 const PORT = process.env.PORT || 4221
 const router = require('./routes/index')
-
+const errorHandler = require('./middleware/errorMiddleware')
 const app = express()
+
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
+
+
+app.use(errorHandler)
 
 
 const start = async () => {
