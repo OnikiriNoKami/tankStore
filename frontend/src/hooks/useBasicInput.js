df24bf9057064cff28c50ec78e383a7385a355ac
OnikiriNoKami <butenko.ily@gmail.com>
2021-08-20 13:53:17 +0300

    basick useInput hook

---
+++ frontend/src/hooks/useBasicInput.js
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+const useBasicInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue)
+    const changeHandler = (e) => {
+        setValue(e.target/value)
+    }
+
+    return {
+        value, onChange: changeHandler
+    }
+}
+
+export default useInput
