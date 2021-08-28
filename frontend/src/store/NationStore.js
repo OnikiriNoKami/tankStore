fd155d1857b68d090b6a0faff29344eaa4939a6a
OnikiriNoKami <butenko.ily@gmail.com>
2021-08-28 16:19:20 +0300

    nations store

---
+++ frontend/src/store/NationStore.js
@@ -0,0 +1,32 @@
+import { NATION_LOADING, NATION_LOADED, NATION_DROP, NATION_SET, NATION_RESET_STATUSES } from "../actions/NationActions"
+
+
+const defaultState = {
+    loading: false,
+    loaded: null,
+    nations: []
+}
+
+export const nationReducer = (state=defaultState, action) => {
+    switch(action.type){
+        case NATION_LOADING:
+            return {...state, loading: action.payload}
+        case NATION_LOADED:
+            return {...state, loaded: action.payload}
+        case NATION_DROP:
+            return {...state, nations: []}
+        case NATION_SET:
+            return {...state, nations: [...state.nations.nations, action.payload]}
+        case NATION_RESET_STATUSES:
+            return {...state, loading: false, loaded:null}
+
+        default:
+            return state
+    }
+}
+
+export const nationsLoading = (payload) => ({type: NATION_LOADING, payload})
+export const nationsLoaded = (payload) => ({type: NATION_LOADED, payload})
+export const nationsDrop = () => ({type: NATION_DROP})
+export const nationsSet = (payload) => ({type: NATION_SET, payload})
+export const nationsResetStatuses = () => ({type: NATION_RESET_STATUSES})
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,12 +5,14 @@ import thunk from 'redux-thunk';
 import { tokenReducer } from "./TokenReducer";
 import { statusReducer } from "./StatusReducer";
 import { messageReducer } from "./MessageStore";
+import { nationReducer } from "./NationStore";
 
 const mainReducer = combineReducers({
     user: userReducer,
     token: tokenReducer,
     status: statusReducer,
-    messages: messageReducer
+    messages: messageReducer,
+    nations: nationReducer
 })
 
 export const store = createStore(mainReducer, composeWithDevTools(applyMiddleware(thunk)))
