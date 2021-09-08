bc4b0dfaa405f0894c00f939dd5950e43018bf5d
OnikiriNoKami <butenko.ily@gmail.com>
2021-09-08 14:37:08 +0300

    module type search by substr route

--- backend/routes/moduleTypeRoute.js
+++ backend/routes/moduleTypeRoute.js
@@ -4,6 +4,7 @@ const authMiddleware = require('../middleware/authMiddleware')
 const router = new Router()
 
 router.get('/', moduleTypeController.getAll)
+router.get('/:search', moduleTypeController.getBySubstring)
 router.post('/',authMiddleware, moduleTypeController.create)
 router.put('/',authMiddleware, moduleTypeController.update)
 router.delete('/',authMiddleware, moduleTypeController.delete)
