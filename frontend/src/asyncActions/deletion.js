940fe644d27ee934d80c92e3e101f19d52e04aab
OnikiriNoKami <butenko.ily@gmail.com>
2021-09-16 13:12:30 +0300

    remove role for user

---
+++ frontend/src/asyncActions/deletion.js
@@ -0,0 +1,20 @@
+import axios from "axios"
+import { failMessage, successMessage } from "../store/MessageStore"
+
+const deleter = (data, path, token) => async(dispatch) => {
+    const headers = {
+        'Authorization': 'jwt '+ token
+    }
+    const body = {
+        ...data
+    }
+    try{
+        const result = await axios.delete(`http://localhost:4221/api/`+path,body,{headers: headers})
+        console.log(result.status)
+        dispatch(successMessage(true))
+    } catch(error){
+        dispatch(failMessage(true))
+    }
+}
+
+export const remoweRoleFromUser = (userId, roleId, token) => (deleter({id: userId, role:roleId}, 'user/roles', token))
