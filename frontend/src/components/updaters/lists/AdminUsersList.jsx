9d3a89a7066547bc6eeb89998988db9855229674
OnikiriNoKami <butenko.ily@gmail.com>
2021-09-17 17:04:59 +0300

    pagination for users list

--- frontend/src/components/updaters/lists/AdminUsersList.jsx
+++ frontend/src/components/updaters/lists/AdminUsersList.jsx
@@ -1,49 +1,92 @@
 import { Container, Grid, Paper, Typography, Modal } from "@material-ui/core";
+import Pagination from "@mui/material/Pagination";
 import { useDispatch, useSelector } from "react-redux";
-import { nationFetch, usersFetch } from "../../../asyncActions/fetcher";
+import { usersFetch } from "../../../asyncActions/fetcher";
 import { useEffect, useState } from "react";
 import SearchBar from "../SearcBar";
-import { nationSearch, usersSearch } from "../../../asyncActions/searchFetcher";
+import { usersSearch } from "../../../asyncActions/searchFetcher";
 import BackdropStyles from "../../../styles/BackdropStyles";
-import { usersSetLimit } from "../../../store/AdminUsers";
+import {
+    usersSetCurrentPage,
+    usersSetLimit,
+} from "../../../store/AdminUsers";
 import UsersRow from "../rows/UsersRow";
+import PaginationStyles from "../../../styles/PaginationStyles";
 
 const AdminUsersList = () => {
     const classes = BackdropStyles();
+    const paginationClasses = PaginationStyles();
     const dispatch = useDispatch();
-    const token = useSelector(state => state.token.token)
+    const loaded = useSelector((state) => state.users.loaded);
+    const token = useSelector((state) => state.token.token);
     const users = useSelector((state) => state.users.users);
-    const limit = useSelector((state) => state.users.limit)
-    const currentPage = useSelector((state) => state.users.page)
-    const offset = useSelector((state) => state.users.offset)
+    const limit = useSelector((state) => state.users.limit);
+    const totalPages = useSelector((state) => state.users.totalPages);
+    const currentPage = useSelector((state) => state.users.page);
+    const offset = useSelector((state) => state.users.offset);
     const [currentUser, setCurrentUser] = useState(null);
+    const [lastQuery, setLastQuery] = useState('');
+    const [lastAction, setLastAction] = useState(null);
     const [open, setOpen] = useState(false);
-
+    dispatch(usersSetLimit(2));
 
     const loadUsers = () => {
-        if(token){
-            dispatch(usersFetch(limit, offset, token));
+        if (token) {
+            setLastAction('load')
+            dispatch(usersSetCurrentPage(1))
+            usersFetchData()
         }
-        
     };
+
+    const usersFetchData = () => {
+        dispatch(usersFetch(limit, offset, token));
+    }
 
     useEffect(() => {
-        loadUsers()        
+        if (token && !loaded) {
+            loadUsers();
+        }
     }, []);
-    useEffect(()=> {
-        loadUsers()
-    }, [token])
+    useEffect(() => {
+        if (token && !loaded) {
+            loadUsers();
+        }
+    }, [token]);
+
+    useEffect(() => {
+        if(lastAction==='load')
+        {
+            usersFetchData();
+        }
+        if(lastAction==='search'){
+            searchFetch(lastQuery);
+        }
+        
+    }, [offset]);
 
     const searchCallBack = (query) => {
-        dispatch(usersSearch(query, limit, offset, token));
+        setLastAction('search')
+        setLastQuery(query);
+        dispatch(usersSetCurrentPage(1))
+        searchFetch(query)
     };
+    
+    const searchFetch = (query) => {
+        dispatch(usersSearch(query, limit, offset, token));
+    }
 
     const handleClick = () => {
-        console.log('click')
-    }
+        console.log(offset);
+    };
 
     const handleClose = () => {
         setOpen(false);
+    };
+
+    const changePage = (event, value) => {
+        if (currentPage !== value) {
+            dispatch(usersSetCurrentPage(value));
+        }
     };
 
     return (
@@ -69,6 +112,17 @@ const AdminUsersList = () => {
                 ) : (
                     <Typography variant="h4">No users</Typography>
                 )}
+                <Grid item xs={6}>
+                    {
+                        users.length !== 0&& <Pagination
+                        classes={{ ul: paginationClasses.ul }}
+                        count={totalPages ? totalPages : 0}
+                        page={currentPage}
+                        onChange={changePage}
+                        />
+                    }                    
+                    
+                </Grid>
             </Grid>
         </Container>
     );
