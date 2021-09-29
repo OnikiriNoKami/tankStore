c5d4b3b2a0313699807e69211353bda54b6cc085
OnikiriNoKami <butenko.ily@gmail.com>
2021-09-29 15:04:30 +0300

    tank deletion request

--- frontend/src/asyncActions/deletion.js
+++ frontend/src/asyncActions/deletion.js
@@ -1,21 +1,30 @@
-import axios from "axios"
-import { failMessage, successMessage } from "../store/MessageStore"
-import { USER_ROLES_PATH } from "../utils/routes"
+import axios from "axios";
+import { failMessage, successMessage } from "../store/MessageStore";
+import { isRequired } from "../utils/errors";
+import { TANK_PATH, USER_ROLES_PATH } from "../utils/routes";
 
-const deleter = (data=null, path=null, token=null) => async(dispatch) => {
-    const headers = {
-        'Authorization': 'jwt '+ token,
-    }
-    const body = {
-        ...data
-    }
-    try{
-        const result = await axios.delete(`http://localhost:4221/api/`+path,{headers: headers, data: body})
-        console.log(result.status)
-        dispatch(successMessage(true))
-    } catch(error){
-        dispatch(failMessage(true))
-    }
-}
+const deleter = (data = null, path = null, token = null) => async (dispatch) => {
+        const headers = {
+            Authorization: "jwt " + token,
+        };
+        const body = {
+            ...data,
+        };
+        try {
+            const result = await axios.delete(
+                `http://localhost:4221/api/` + path,
+                { headers: headers, data: body }
+            );
+            console.log(result.status);
+            dispatch(successMessage(true));
+        } catch (error) {
+            dispatch(failMessage(true));
+        }
+    };
 
-export const removeRoleFromUser = (userId, roleId, token) => (deleter({id: userId, role:roleId}, USER_ROLES_PATH, token))
+export const removeRoleFromUser = (userId, roleId, token) =>
+    deleter({ id: userId, role: roleId }, USER_ROLES_PATH, token);
+
+export const deleteTank = (tankId=isRequired('Tank id'), token=isRequired('Token')) => deleter({id: tankId}, TANK_PATH, token);
+
+
