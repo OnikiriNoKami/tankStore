61e49e2a73b4d7e2313ef2d8436e7205a6148fda
OnikiriNoKami <butenko.ily@gmail.com>
2021-10-11 17:49:14 +0300

    grid style

--- frontend/src/styles/SelectStyles.js
+++ frontend/src/styles/SelectStyles.js
@@ -4,15 +4,17 @@ const ITEM_PADDING_TOP = 8;
 
 const SelectStyles = makeStyles((theme) => {
     return {
-        MenuProps: {
-            PaperProps: {
-                style: {
-                    maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-                    width: 250,
-                },
-            },
-        },
+        
     };
 });
+
+export const MenuProps = {    
+        PaperProps: {
+            style: {
+                maxHeight: ITEM_HEIGHT * 8 + ITEM_PADDING_TOP,
+                width: 250,
+            },
+        },
+}
 
 export default SelectStyles
