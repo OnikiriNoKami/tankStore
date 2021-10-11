8ce7494def0c7a09960f31a1e84ba43d5719a351
OnikiriNoKami <butenko.ily@gmail.com>
2021-10-11 17:53:32 +0300

    new folder

---
+++ frontend/src/components/utils/SelectMultipleCheck.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import ListItemText from "@material-ui/core/ListItemText";
+import Select from "@material-ui/core/Select";
+import Checkbox from "@material-ui/core/Checkbox";
+import {MenuProps} from "../../styles/SelectStyles";
+
+export default function SelectMultipleCheck({list,values,selectLabel='', onChange, ...rest}) {
+    const createLabel = (selected) => {
+        let label = [];
+        selected.forEach((id)=>{
+            const element = list.find((element)=>{
+                if(element.id === id){
+                    return true;
+                }
+            })
+
+            label.push(element.title)
+        })
+        return label.join(', ');
+    }
+
+    return (
+        <div>
+            <FormControl style={{width:'100%' }}>
+                <InputLabel>{selectLabel}</InputLabel>
+                <Select
+                    multiple
+                    value={values}
+                    onChange={onChange}
+                    //input={<OutlinedInput label="Tag" />}
+                    renderValue={createLabel}
+                    MenuProps={MenuProps}                
+                >
+                    {list?.map((element) => (
+                        <MenuItem key={element.id} value={element.id}>
+                            <Checkbox
+                                checked={values.indexOf(element.id) > -1}
+                            />
+                            <ListItemText primary={element.title} />
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        </div>
+    );
+}
