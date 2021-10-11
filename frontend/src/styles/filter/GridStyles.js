10fa0f3fe18975578aa0de772a82941f6f38c196
OnikiriNoKami <butenko.ily@gmail.com>
2021-10-11 17:52:02 +0300

    grid styles

---
+++ frontend/src/styles/filter/GridStyles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+
+const GridStyles = makeStyles((theme)=> {
+    return {
+        GridMarginBottom: {
+            marginBottom: 10,
+        }
+    }
+})
+
+export default GridStyles
