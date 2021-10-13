8ef547f9db50ede1ec4f97e0c51dbe775347bf0f
OnikiriNoKami <butenko.ily@gmail.com>
2021-10-13 18:15:17 +0300

    style for number input

---
+++ frontend/src/styles/filter/NumberInputStyles.js
@@ -0,0 +1,21 @@
+import { makeStyles } from "@material-ui/core";
+
+const NumberInputStyles = makeStyles((theme) => {
+    return {
+      input: {
+        '& input[type=number]': {
+            '-moz-appearance': 'textfield'
+        },
+        '& input[type=number]::-webkit-outer-spin-button': {
+            '-webkit-appearance': 'none',
+            margin: 0
+        },
+        '& input[type=number]::-webkit-inner-spin-button': {
+            '-webkit-appearance': 'none',
+            margin: 0
+        }
+    },
+    }
+})
+
+export default NumberInputStyles
