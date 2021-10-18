ff608a8cd8e9f07735664004ff208c27e8e07370
OnikiriNoKami <butenko.ily@gmail.com>
2021-10-18 15:17:19 +0300

    main image controller

---
+++ backend/controllers/mainImageController.js
@@ -0,0 +1,79 @@
+const uuid = require("uuid");
+const { MainImage } = require("../models/models");
+const path = require("path");
+const ApiError = require("../error/ApiError");
+const messages = require("../message/databaseRelated");
+const fs = require("fs");
+//const { Op } = require("sequelize");
+
+class ImageController {
+    async create(req, res, next) {
+        try {
+            const { tankId } = req.body;
+            const { file } = req.files;
+            let fileName = uuid.v4() + ".jpg";
+
+            file.mv(path.resolve(__dirname, "..", "static", fileName));
+            const img = await MainImage.create({
+                tankId,
+                title: fileName,
+            });
+
+            return res.status(201).json(img);
+        } catch (err) {
+            return next(err);
+        }
+    }
+
+    async getById(req, res, next) {
+        try {
+            const { tankId } = req.params;
+            const images = await MainImage.findOne({
+                attributes: ["id", "title", "tankId"],
+                where: {
+                    tankId: tankId,
+                },
+            });
+
+            return res.json(images);
+        } catch (err) {
+            return next(err);
+        }
+    }
+
+    async delete(req, res, next) {
+        try {
+            const { id } = req.body;
+            const image = await MainImage.findOne({
+                where: {
+                    id: id,
+                },
+            });
+
+            if (image === null) {
+                return next(ApiError.badRequest(messages.NOT_IN_DATABASE));
+            } else {
+                try {
+                    fs.unlink(
+                        path.resolve(__dirname, "..", "static", image.title),
+                        (errs) => {
+                            if (errs) {
+                                return res.json({ message: errs.message });
+                            }
+                            console.log(`Image ${image.title} deleted`);
+                        }
+                    );
+                    await image.destroy();
+                    return res.json({ message: messages.DELETION_SUCCESS });
+                } catch (err) {
+                    return res.json({ message: err.message });
+                }
+            }
+        } catch {
+            return next(err);
+        }
+    }
+
+}
+
+module.exports = new ImageController();
