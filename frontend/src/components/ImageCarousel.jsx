7f4bbf65a9a80e91553dfd751e6236b7860c512b
OnikiriNoKami <butenko.ily@gmail.com>
2021-10-19 15:12:56 +0300

    added checkbox for images

--- frontend/src/components/ImageCarousel.jsx
+++ frontend/src/components/ImageCarousel.jsx
@@ -1,14 +1,24 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../override.css";
 import { Carousel } from "react-responsive-carousel";
+import { Checkbox, FormControlLabel } from "@material-ui/core";
 
-const ImageCarousel = ({ images = [], ...props }) => {
+const ImageCarousel = ({ images = [],handleChecked,checkedMain, ...props }) => {
     return (
         <Carousel {...props}>
             {images.value.length !== 0 ? (
                 images.value.map((image) => (
                     <div key={image.url}>
                         <img src={image.url} />
+                            <FormControlLabel
+                                label="As main image"
+                                style={{
+                                    position: "absolute",
+                                    right: 25,
+                                    top: 5,
+                                }}
+                                control={<Checkbox checked={image.id===checkedMain} onChange={()=>handleChecked(image.id)} />}
+                            />
                     </div>
                 ))
             ) : (
